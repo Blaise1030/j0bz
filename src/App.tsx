632d327bb6588b685116c2d348dfcb59cc12b507
Blaise1030 <61928648+Blaise1030@users.noreply.github.com>
2021-12-06 22:58:03 +0800

    Removed error

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
 import React from "react";
 import "./App.css";
-// import { DOMMessage, DOMMessageResponse } from "./types";
+import { DOMMessage, DOMMessageResponse } from "./types";
 
 function App() {
-  // const [headlines, setHeadlines] = React.useState<string[]>([]);
+  const [headlines, setHeadlines] = React.useState<string[]>([]);
 
-  // React.useEffect(() => {
-  //   setInterval(() => {
-  //     console.log("Im runn");
-  //     chrome.tabs &&
-  //       chrome.tabs.query(
-  //         {
-  //           active: true,
-  //           currentWindow: true,
-  //         },
-  //         (tabs) => {
-  //           console.log("Im runn Blaise");
-  //           chrome.tabs.sendMessage(
-  //             tabs[0].id || 0,
-  //             { type: "GET_DOM" } as DOMMessage,
-  //             (response: DOMMessageResponse) => {
-  //               setTitle(response.title);
-  //               setHeadlines(response.headlines);
-  //             }
-  //           );
-  //         }
-  //       );
-  //   }, 1000);
-  // }, []);
+  React.useEffect(() => {
+    // run on every 1 s
+    setInterval(() => {
+      chrome.tabs &&
+        chrome.tabs.query(
+          {
+            active: true,
+            currentWindow: true,
+          },
+          (tabs) => {
+            chrome.tabs.sendMessage(
+              tabs[0].id || 0,
+              { type: "GET_DOM" } as DOMMessage,
+              (response: DOMMessageResponse) => {
+                setHeadlines(response.headlines);
+              }
+            );
+          }
+        );
+    }, 1000);
+  }, []);
 
   return <div className="App"></div>;
 }
--- src/chromeServices/DOMEvaluator.ts
+++ src/chromeServices/DOMEvaluator.ts
@@ -5,18 +5,18 @@ const messagesFromReactAppListener = (
   sender: chrome.runtime.MessageSender,
   sendResponse: (response: DOMMessageResponse) => void
 ) => {
-  const response: DOMMessageResponse = {
-    headlines: Array.from(document.getElementsByTagName<"a">("a")).map((a) => {
-      const string = a.href.toString();
-      if (
-        string.includes("https://myjobstreet.jobstreet.com.my") ||
-        string.includes("http://myjobstreet.jobstreet.com.my")
-      ) {
-        return a.href;
-      }
-    }),
-  };
-  sendResponse(response);
+  // const response: DOMMessageResponse = {
+  //   headlines: Array.from(document.getElementsByTagName<"a">("a")).map((a) => {
+  //     const string = a.href.toString();
+  //     if (
+  //       string.includes("https://myjobstreet.jobstreet.com.my") ||
+  //       string.includes("http://myjobstreet.jobstreet.com.my")
+  //     ) {
+  //       return a.href;
+  //     }
+  //   }),
+  // };
+  // sendResponse(response);
 };
 
 /**
