31e282cf27371bff3f613f9a8f0fa1cd1a58b615
Wei Chun <tanweichun@outlook.com>
2021-12-08 17:25:46 +0000

    Co-authored-by: SynxC <SynxC@users.noreply.github.com> Co-authored-by: Blaise1030 <Blaise1030@users.noreply.github.com> Co-authored-by: Chee Min Hao <CheeMinHao@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { DOMMessage, DOMMessageResponse } from "./types";
 import {
   Box,
   ChakraProvider,
-  FormControl,
-  FormLabel,
-  GridItem,
-  Heading,
-  Input,
-  SimpleGrid,
-  Stack,
-  useColorModeValue,
-  Text,
-  chakra,
-  Flex,
-  Icon,
-  VisuallyHidden,
-  IconButton,
+  HStack,
+  Progress,
+  Center,
 } from "@chakra-ui/react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import WorkExperienceForm from "./Components/WorkExperienceForm";
+import PersonalInformation from "./Components/PersonalInformation";
 
 function App() {
   const [headlines, setHeadlines] = React.useState<string[]>([]);
-
-  React.useEffect(() => {
+  const [section, setSection] = useState(1);
+  const [basicInformation, setBasicInformation] = useState({});
+  const [workExperience, setWorkExperience] = useState<any>([]);
+  const isDev = false;
+  useEffect(() => {
     // run on every 1 s
     setInterval(() => {
       chrome.tabs &&
@@ -47,300 +40,109 @@ function App() {
     }, 1000);
   }, []);
 
+  const saveCurrent = () => {
+    const saveIntoLocalStorage = (category: string, toStore: object) => {
+      localStorage.setItem(category, JSON.stringify(toStore));
+    };
+    const saveIntoChromeStorage = (category: string, toStore: object) => {
+      console.log("this is stored", { [category]: toStore });
+      chrome.storage.sync.set({ [category]: toStore });
+    };
+    switch (section) {
+      case 1:
+        isDev
+          ? saveIntoLocalStorage("personalInformation", basicInformation)
+          : saveIntoChromeStorage("personalInformation", basicInformation);
+        break;
+      case 2:
+        isDev
+          ? saveIntoLocalStorage("workExperience", workExperience)
+          : saveIntoChromeStorage("workExperience", workExperience);
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    const getFromLocalStorage = async (category: string) => {
+      const localStored = localStorage.getItem(category);
+      const storedState = isDev
+        ? localStored != null
+          ? JSON.parse(localStored)
+          : {}
+        : await chrome.storage.sync.get([category]);
+      console.log(storedState, "Retreive");
+      if (category === "personalInformation")
+        setBasicInformation(
+          isDev ? storedState : storedState.personalInformation
+        );
+      else
+        setWorkExperience(
+          isDev ? storedState : storedState.workExperience ?? []
+        );
+    };
+
+    switch (section) {
+      case 1:
+        getFromLocalStorage("personalInformation");
+        break;
+      case 2:
+        getFromLocalStorage("workExperience");
+        break;
+      default:
+        break;
+    }
+  }, [section]);
+
   return (
     <ChakraProvider>
       <div className="App">
-        <Box px={4} rounded={10}>
-          <Heading fontSize="lg" fontWeight="medium" lineHeight="6">
-            Personal Information
-          </Heading>
-          <Text
-            mt={1}
-            fontSize="sm"
-            color={useColorModeValue("gray.600", "gray.400")}
-          >
-            Use a permanent address where you can receive mail.
-          </Text>
+        <Box w="100%">
+          <HStack spacing="10px" width="100%">
+            <Progress
+              width="75%"
+              size="sm"
+              borderRadius="4"
+              value={parseInt(((section / 2) * 100).toString())}
+              my={4}
+            />
+            <Button colorScheme="red" onClick={saveCurrent} width="25%">
+              Save this thing
+            </Button>
+          </HStack>
+          {section == 1 && (
+            <PersonalInformation
+              currentState={basicInformation}
+              setState={setBasicInformation}
+            />
+          )}
+          {section == 2 && (
+            <WorkExperienceForm
+              experience={workExperience}
+              setExperience={setWorkExperience}
+            />
+          )}
+          <Center width="100%">
+            {section != 1 && (
+              <Button
+                colorScheme="teal"
+                onClick={() => setSection(section - 1)}
+                width="40%"
+                mr={8}
+              >
+                Previous
+              </Button>
+            )}
+            <Button
+              colorScheme="teal"
+              onClick={() => setSection(section + 1)}
+              width="40%"
+            >
+              Next
+            </Button>
+          </Center>
         </Box>
-
-        <Stack
-          px={4}
-          py={5}
-          p={[null, 6]}
-          bg={useColorModeValue("white", "gray.700")}
-          spacing={6}
-        >
-          <SimpleGrid columns={6} spacing={6}>
-            <FormControl as={GridItem} colSpan={[6, 3]}>
-              <FormLabel
-                htmlFor="first_name"
-                fontSize="sm"
-                fontWeight="md"
-                color={useColorModeValue("gray.700", "gray.50")}
-              >
-                First name
-              </FormLabel>
-              <Input
-                type="text"
-                name="first_name"
-                id="first_name"
-                autoComplete="given-name"
-                mt={1}
-                focusBorderColor="brand.400"
-                shadow="sm"
-                size="sm"
-                w="full"
-                rounded="md"
-              />
-            </FormControl>
-
-            <FormControl as={GridItem} colSpan={[6, 3]}>
-              <FormLabel
-                htmlFor="last_name"
-                fontSize="sm"
-                fontWeight="md"
-                color={useColorModeValue("gray.700", "gray.50")}
-              >
-                Last name
-              </FormLabel>
-              <Input
-                type="text"
-                name="last_name"
-                id="last_name"
-                autoComplete="family-name"
-                mt={1}
-                focusBorderColor="brand.400"
-                shadow="sm"
-                size="sm"
-                w="full"
-                rounded="md"
-              />
-            </FormControl>
-
-            <FormControl as={GridItem} colSpan={[6, 3]}>
-              <FormLabel
-                htmlFor="email_address"
-                fontSize="sm"
-                fontWeight="md"
-                color={useColorModeValue("gray.700", "gray.50")}
-              >
-                Email address
-              </FormLabel>
-              <Input
-                type="text"
-                name="email_address"
-                id="email_address"
-                autoComplete="email"
-                mt={1}
-                focusBorderColor="brand.400"
-                shadow="sm"
-                size="sm"
-                w="full"
-                rounded="md"
-              />
-            </FormControl>
-
-            <FormControl as={GridItem} colSpan={[6, 3]}>
-              <FormLabel
-                htmlFor="phone_number"
-                fontSize="sm"
-                fontWeight="md"
-                color={useColorModeValue("gray.700", "gray.50")}
-              >
-                Phone Number
-              </FormLabel>
-              <Input
-                type="text"
-                name="phone_number"
-                id="phone_number"
-                autoComplete="phone no."
-                mt={1}
-                focusBorderColor="brand.400"
-                shadow="sm"
-                size="sm"
-                w="full"
-                rounded="md"
-              />
-            </FormControl>
-
-            <FormControl as={GridItem} colSpan={6}>
-              <FormLabel
-                htmlFor="street_address"
-                fontSize="sm"
-                fontWeight="md"
-                color={useColorModeValue("gray.700", "gray.50")}
-              >
-                Street address
-              </FormLabel>
-              <Input
-                type="text"
-                name="street_address"
-                id="street_address"
-                autoComplete="street-address"
-                mt={1}
-                focusBorderColor="brand.400"
-                shadow="sm"
-                size="sm"
-                w="full"
-                rounded="md"
-              />
-            </FormControl>
-
-            <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
-              <FormLabel
-                htmlFor="city"
-                fontSize="sm"
-                fontWeight="md"
-                color={useColorModeValue("gray.700", "gray.50")}
-              >
-                City
-              </FormLabel>
-              <Input
-                type="text"
-                name="city"
-                id="city"
-                autoComplete="city"
-                mt={1}
-                focusBorderColor="brand.400"
-                shadow="sm"
-                size="sm"
-                w="full"
-                rounded="md"
-              />
-            </FormControl>
-
-            <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-              <FormLabel
-                htmlFor="state"
-                fontSize="sm"
-                fontWeight="md"
-                color={useColorModeValue("gray.700", "gray.50")}
-              >
-                State / Province
-              </FormLabel>
-              <Input
-                type="text"
-                name="state"
-                id="state"
-                autoComplete="state"
-                mt={1}
-                focusBorderColor="brand.400"
-                shadow="sm"
-                size="sm"
-                w="full"
-                rounded="md"
-              />
-            </FormControl>
-
-            <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-              <FormLabel
-                htmlFor="postal_code"
-                fontSize="sm"
-                fontWeight="md"
-                color={useColorModeValue("gray.700", "gray.50")}
-              >
-                ZIP / Postal
-              </FormLabel>
-              <Input
-                type="text"
-                name="postal_code"
-                id="postal_code"
-                autoComplete="postal-code"
-                mt={1}
-                focusBorderColor="brand.400"
-                shadow="sm"
-                size="sm"
-                w="full"
-                rounded="md"
-              />
-            </FormControl>
-          </SimpleGrid>
-        </Stack>
-        <Box
-          px={{ base: 4, sm: 6 }}
-          py={3}
-          bg={useColorModeValue("gray.50", "gray.900")}
-          textAlign="right"
-        >
-          <Button
-            type="submit"
-            colorScheme="brand"
-            _focus={{ shadow: "" }}
-            fontWeight="md"
-          >
-            Save
-          </Button>
-        </Box>
-        <FormControl>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="md"
-            color={useColorModeValue("gray.700", "gray.50")}
-          >
-            Your CV
-          </FormLabel>
-          <Flex
-            mt={1}
-            justify="center"
-            px={6}
-            pt={5}
-            pb={6}
-            borderWidth={2}
-            borderColor={useColorModeValue("gray.300", "gray.500")}
-            borderStyle="dashed"
-            rounded="md"
-          >
-            <Stack spacing={1} textAlign="center">
-              <Icon
-                mx="auto"
-                boxSize={12}
-                color={useColorModeValue("gray.400", "gray.500")}
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 48 48"
-                aria-hidden="true"
-              >
-                <path
-                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </Icon>
-              <Flex
-                fontSize="sm"
-                color={useColorModeValue("gray.600", "gray.400")}
-                alignItems="baseline"
-              >
-                <chakra.label
-                  htmlFor="file-upload"
-                  cursor="pointer"
-                  rounded="md"
-                  fontSize="md"
-                  color={useColorModeValue("brand.600", "brand.200")}
-                  pos="relative"
-                  _hover={{
-                    color: useColorModeValue("brand.400", "brand.300"),
-                  }}
-                >
-                  <span>Upload a file</span>
-                  <VisuallyHidden>
-                    <input id="file-upload" name="file-upload" type="file" />
-                  </VisuallyHidden>
-                </chakra.label>
-                <Text pl={1}>or drag and drop</Text>
-              </Flex>
-              <Text
-                fontSize="xs"
-                color={useColorModeValue("gray.500", "gray.50")}
-              >
-                PNG, JPG, GIF up to 10MB
-              </Text>
-            </Stack>
-          </Flex>
-        </FormControl>
-
-        <WorkExperienceForm />
       </div>
     </ChakraProvider>
   );
--- src/Components/WorkExperienceForm.tsx
+++ src/Components/WorkExperienceForm.tsx
@@ -1,7 +1,256 @@
-import React from "react";
+import { useColorModeValue } from "@chakra-ui/color-mode";
+import { SimpleGrid, GridItem, Box } from "@chakra-ui/layout";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  VStack,
+  Select,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { SmallAddIcon, ChevronDownIcon } from "@chakra-ui/icons";
 
-const WorkExperienceForm = () => {
-  return <div>Thsi is the work experice from</div>;
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Declare Work Experience type
+export type WorkExperience = {
+  id: number;
+  jobTitle: string;
+  companyName: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  industry: string;
 };
 
-export default WorkExperienceForm;
+// Form UI which includes all text based input components
+const TextInput = ({
+  title,
+  type,
+  value,
+  onChange,
+}: {
+  onChange: (a: string) => void;
+  title: string;
+  type: string;
+  value: string;
+}) => {
+  return (
+    <>
+      <FormControl as={GridItem} colSpan={[6, 3]}>
+        {/* First Name Input */}
+        <FormLabel
+          htmlFor="first_name"
+          fontSize="sm"
+          fontWeight="md"
+          color={useColorModeValue("gray.700", "gray.50")}
+        >
+          {title}
+        </FormLabel>
+
+        {/* Given name Input */}
+        <Input
+          onChange={(e) => {
+            console.log(e);
+            onChange(e.target.value);
+          }}
+          value={value}
+          type={type}
+          autoComplete="given-name"
+          mt={1}
+          focusBorderColor="brand.400"
+          shadow="sm"
+          size="sm"
+          w="full"
+          rounded="md"
+        />
+      </FormControl>
+    </>
+  );
+};
+
+const WorkExperienceForm = ({
+  updateStore,
+  value,
+}: {
+  updateStore: (a: WorkExperience) => void;
+  value: WorkExperience;
+}) => {
+  const updateField = (key: string, newValue: string) => {
+    updateStore({ ...value, [key]: newValue });
+  };
+
+  return (
+    <SimpleGrid columns={6} spacing={6} p={3}>
+      <TextInput
+        onChange={(change) => updateField("jobTitle", change)}
+        value={value.jobTitle}
+        title={"Job Title"}
+        type={"text"}
+      />
+      <TextInput
+        onChange={(change) => updateField("companyName", change)}
+        value={value.companyName}
+        title={"Company Name"}
+        type={"text"}
+      />
+      <TextInput
+        onChange={(change) => updateField("location", change)}
+        value={value.location}
+        title={"Location"}
+        type={"text"}
+      />
+      <TextInput
+        onChange={(change) => updateField("startDate", change)}
+        value={value.startDate}
+        title={"Start date"}
+        type={"date"}
+      />
+      <TextInput
+        onChange={(change) => updateField("endDate", change)}
+        value={value.endDate}
+        title={"End date"}
+        type={"date"}
+      />
+      <IndustryDropdown
+        onSelect={(newIndustry: string) => updateField("industry", newIndustry)}
+        value={value.industry}
+      />
+    </SimpleGrid>
+  );
+};
+
+// Work Experience View Handler for its UI dat
+const WorkExperienceView = ({
+  experience,
+  setExperience,
+}: {
+  experience: WorkExperience[];
+  setExperience: (n: WorkExperience[]) => void;
+}) => {
+  console.log("now", experience);
+  // Event Handler that handles event when user adds an experience
+  const onAddExperienceClick = () =>
+    setExperience([
+      {
+        id: Date.now(),
+        jobTitle: "",
+        companyName: "",
+        location: "",
+        startDate: "",
+        endDate: "",
+        industry: "",
+      },
+      ...experience,
+    ]);
+
+  // Event Handler that handles event to set the experience
+  const onSetExperience = (newObject: WorkExperience) =>
+    setExperience(
+      experience?.map((e) => (e.id === newObject.id ? newObject : e))
+    );
+
+  return (
+    <VStack alignItems={"start"} justifyContent={"start"}>
+      <Button onClick={onAddExperienceClick}>
+        <SmallAddIcon w={30} h={30} /> <span>Add New Experience</span>
+      </Button>
+      {experience?.map((value, index) => (
+        <WorkExperienceForm
+          updateStore={onSetExperience}
+          key={index}
+          value={value}
+        />
+      ))}
+    </VStack>
+  );
+};
+
+// Industry Dropdown Function
+const IndustryDropdown = ({
+  onSelect,
+  value,
+}: {
+  onSelect: (e: any) => void;
+  value?: string;
+}) => {
+  // List of Industries (Hard Coded)
+  const industries = [
+    "Advertising & Marketing",
+    "Aerospace",
+    "Agriculture",
+    "Computer & technology",
+    "Construction",
+    "Education",
+    "Energy",
+    "Entertainment",
+    "Fashion",
+    "Finance & economic",
+    "Food & beverage",
+    "Health",
+    "Hospitality",
+    "Manufacturing",
+    "Media & News",
+    "Mining",
+    "Pharmaceutical",
+    "Telecommunication",
+    "Transportation",
+  ];
+  return (
+    // Returns a list of industries
+    <Select
+      as={GridItem}
+      colSpan={[6, 3]}
+      name="industry"
+      id="industry"
+      placeholder="Select industry"
+      onChange={(event) => {
+        console.log(event.target.value);
+        onSelect(event.target.value);
+      }}
+      value={value?.length === 0 ? industries[0] : value}
+    >
+      {industries.map((industry: string) => (
+        <option key={industry}>{industry}</option>
+      ))}
+    </Select>
+  );
+};
+
+// Education Dropdown Function
+const EducationDropDown = ({
+  onSelect,
+  value,
+}: {
+  onSelect: (e: any) => void;
+  value: string;
+}) => {
+  // List of Education levels (Hard Coded)
+  const education = [
+    "No formal education",
+    "Primary education",
+    "Secondary education",
+    "GED",
+    "Vocational qualification",
+    "Bachelor's degree",
+    "Master's degree",
+    "Doctorate or higher",
+  ];
+  return (
+    // Returns a list of education
+    <Select
+      name="education"
+      id="education"
+      placeholder="Select education"
+      onChange={(event) => onSelect(event.target.value)}
+      value={value.length === 0 ? education[0] : value}
+    >
+      {education.map((education: string) => (
+        <option key={education}>{education}</option>
+      ))}
+    </Select>
+  );
+};
+
+export default WorkExperienceView;
